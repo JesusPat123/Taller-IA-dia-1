e58c12dae4e0a5c30366c3c435b71fa5371e2b34
Jesus Gabriel Pat Canul <l25070075@valladolid.tecnm.mx>
2025-11-26 08:53:45 -0600

    Jesus Gabriel Pat Canul

--- logica-dia-2/script.js
+++ logica-dia-2/script.js
@@ -1,3 +1,32 @@
 //Ejercicio: Arrays y Objetos
 //1. array(listas)
 //Crea una lista de tus tres comidas favoritas
+var comidasFavoritas = ["Pizza", "Sushi", "Tacos"];
+
+//2. objetos (kay y vaule)
+//Crea un objeto que represente a una persona con las propiedades: nombre, edad y ciudad
+var persona = {
+  nombre: "Nombre: Juan",
+  edad: 30,
+  ciudad: "Madrid",
+  habilidades: ["Programación", "dibujo", "cocina"],
+  estatura: 1.75,
+  programador: true,
+};
+
+//como accedo a la propiedad de nombre del objeto persona
+console.log(persona.nombre);
+
+//como accedo a la propiedad habilidades del objeto persona
+console.log(persona.habilidades);
+
+//como accedo a la habilidad del dibujo del objeto persona
+console.log(persona.habilidades[2]);
+
+//3. array de objetos
+//Crea una lista de tres alumnos (objetos) con nombre y calificación
+var alumnos = [
+  { nombre: "Ana", calificacion: 85 },
+  { nombre: "Luis", calificacion: 92 },
+  { nombre: "Marta", calificacion: 78 },
+];
---
+++ taller.js
@@ -0,0 +1,7 @@
+//Escribe algo en la consola
+console.log("Hola, este es mi primer código en JavaScript");
+
+//"¿Qué es un commit y por qué es importante?"
+
+
+
